24e976effbb194e3378523c09abeff2b55b000e1
Giovana Duarte <you@example.com>
2021-11-22 16:57:26 -0300

    Primeiro commit

---
+++ Palindromo/Palindromo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace projeto_txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Texto = new StreamReader(@"C:\Users\Giovana Duarte\Videos\Código\Projeto Palindromo\Palindromo\Texto.txt");
+            StreamWriter sw = new StreamWriter(@"C:\Users\Giovana Duarte\Videos\Código\Projeto Palindromo\Palindromo\Resultado.txt");
+            string Resultado;
+
+            using (Texto)
+            {
+                while (!Texto.EndOfStream)
+                {
+                    var LinhaTexto = Texto.ReadLine().ToUpper();
+                    var Reverso = InverterPalavra(LinhaTexto).Replace(" ", "");
+                    Console.WriteLine(LinhaTexto);
+
+
+                    if (LinhaTexto.Replace(" ", "").Equals(Reverso))
+                    {
+                        Resultado = $"A  frase {LinhaTexto} é um palindromo";
+                        sw.WriteLine(Resultado);
+                    }
+                    else
+                    {
+                        Resultado = $"A  frase {LinhaTexto} não é um palindromo";
+                        sw.WriteLine(Resultado);
+                    }
+
+                }
+                sw.Close();
+            }
+        }
+        public static string InverterPalavra(string LinhaTexto)
+        {
+            char[] ArrayChar = LinhaTexto.ToCharArray();
+            Array.Reverse(ArrayChar);
+            return new string(ArrayChar);
+        }
+    }
+}
